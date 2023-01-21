35ebd14a668b9d5ace016fa31532394715a6ad26
Shanmugapriya Ravi <shanmupriya96@outlook.com>
2023-01-21 11:48:57 -0800

    Tutorial-3:Functional Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-  const handleNameChange =() => {
-    const names = ['Shan','Shanmu','Priya'];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-
-  }
+ 
   return (
     <div className="App">
       <Header />
-    
+      <Content />
+      <Footer />
       
     </div>
   );
